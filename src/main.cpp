d34cc16fc237b84fc0f7bdae644c14d3f73f6032
egermanas34-beep <egermanas34@gmail.com>
2026-05-31 15:51:13 +0300

    prideta vartotojo sasaja

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,35 @@
 #include "word.h"
 #include "wordcounter.h"
 #include "urlfinder.h"
-#include "funkcijos.h"
+
 #include "windows.h"
 int main()
 {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
-    /*map<string, Word> zodziai;
-    nuskaitymas("tekstas.txt", zodziai);
-    isvedimas(zodziai);*/
+    
     WordCounter wc;
-    wc.nuskaitymas("tekstas.txt");
-    wc.isvedimas("rezultatai.txt");
     UrlFinder uf;
-    uf.nuskaitymas("tekstas2.txt");
-    uf.isvedimas("rezultatai2.txt");
+    
+    cout<< " Ka jus norite padaryti? \n 1. Rasti zodzius ir ju skaiciu tekste \n 2. Rasti URL adresus tekste \n";
+    int pasirinkimas;
+    std::cin >> pasirinkimas;
+    switch(pasirinkimas)
+    {
+        case 1:
+            wc.nuskaitymas("tekstas.txt");
+            wc.isvedimas("rezultatai.txt");
+            break;
+        case 2:
+            uf.nuskaitymas("tekstas2.txt");
+            uf.isvedimas("rezultatai2.txt");
+            break;
+        default:
+            cout << "Neteisingas pasirinkimas. Programa baigiama." << endl;
+    }
+    cout<<"Programa baigta. Rezultatai issaugoti atitinkamuose failuose."<<endl;
+    cout<<"Paspauskite Enter, kad uzdarytumete programa..."<<endl;
+    std::cin.ignore();
+    std::cin.get();
     return 0;
 }
