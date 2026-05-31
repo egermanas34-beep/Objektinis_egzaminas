2bcda8978ee35b0ebe153fe6fc961e00e8817acd
egermanas34-beep <egermanas34@gmail.com>
2026-05-31 15:45:14 +0300

    neteisingai buvo padaryti move konst

--- include/funkcijos.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "word.h"
-
-string arZodis(const string& zodis);
-void nuskaitymas(const string& failas, map<string, Word>& zodziai);
-void isvedimas(const map<string, Word>& zodziai);
